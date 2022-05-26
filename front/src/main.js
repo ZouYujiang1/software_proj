802e7b16b417fb7d1341526e7a6f5ab405046786
ljh2019211415 <2717983753@qq.com>
2022-05-26 21:57:35 +0800

    update login

--- front/src/main.js
+++ front/src/main.js
@@ -3,17 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
-import 'element-plus/lib/theme-chalk/index.css'
+import 'element-plus/dist/index.css'
 import axios from 'axios'
 import "./axios"
 import $ from 'jquery'
-import SuiVue from 'semantic-ui-vue'
-import 'semantic-ui-css/semantic.min.css'
+
+import Particles from 'particles.vue3'
 const app=createApp(App)
 app.config.globalProperties.$axios=axios
 app.config.globalProperties.$=$
 app.use(store)
     .use(router)
     .use(ElementPlus)
-    .use(SuiVue)
+
+
+    .use(Particles)
     .mount('#app')
