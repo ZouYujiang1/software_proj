722d08f62421ff0466a0c43400201a499dc8efe0
buptkt <68584203+buptkt@users.noreply.github.com>
2022-06-13 13:28:31 +0800

    update chargers part and server part

--- front/src/views/Ch_statistic.vue
+++ front/src/views/Ch_statistic.vue
@@ -82,8 +82,9 @@ export default {
     get_data(){
       let vm = this
         axios
-            .get('http://localhost:8081/chargers')
+            .get('http://127.0.0.1:5000/admin/charger/statistic')
             .then(function (response){
+              response = JSON.parse(response)
               vm.message = response;
               vm.current = response[0];
               for(const i in response){
