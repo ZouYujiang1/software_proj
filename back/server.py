283d53c39280be00bbc620999522aa889bfd952e
TLCFY <978979378@qq.com>
2022-06-15 00:26:54 +0800

    update admin return info forms

--- back/server.py
+++ back/server.py
@@ -299,17 +299,17 @@ def usrEndCharging():
 def usrGetOrderInfo():
     usrID = request.json['id']
     orderID = usrActiveOrder[usrID]
-    return json.dumps(str(db.getOrder(orderID)))
+    return json.dumps({'data':db.getOrder(orderID)})
 
 
 @app.route("/admin/usr-info", methods=['POST'])
 def adminUsrInfo():
-    return json.dumps(str(db.getAllUserInfo()))
+    return json.dumps({'data':db.getUserInfo()})
 
 
 @app.route("/admin/queue-info", methods=['POST'])
 def adminQueuingUserInfo():
-    return json.dumps(str(db.getAllQueuingUserInfo()))
+    return json.dumps({'data':db.getAllQueuingUserInfo()})
 
 
 @app.route("/admin/charger/status", methods=['POST', 'GET'])
