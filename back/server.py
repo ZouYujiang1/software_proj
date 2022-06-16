b8b3ba346632e52abbc035b4f6b421269807c40e
buptkt <68584203+buptkt@users.noreply.github.com>
2022-06-16 16:10:24 +0800

    Update server.py

--- back/server.py
+++ back/server.py
@@ -270,7 +270,7 @@ def usrStartCharging():
 
         startUpTime=datetime.now()  # 默认使用系统时间作为开始充电时间
         # 测试时前段会传递开始时间
-        if 'year' in request.json.__dict__.keys():
+        if 'year' in request.json.keys():
             startUpTime = datetime(year=int(request.json['year']),
                                     month=int(request.json['month']),
                                     day=int(request.json['day']),
@@ -300,7 +300,7 @@ def usrEndCharging():
     # 默认使用系统时间作为结束时间
     chargingEndTime = datetime.now()
     # 测试时前段会传递结束时间
-    if 'year' in request.json.__dict__.keys():
+    if 'year' in request.json.keys():
         chargingEndTime = datetime(year=int(request.json['year']),
                                 month=int(request.json['month']),
                                 day=int(request.json['day']),
